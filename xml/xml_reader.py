5edf1fab053523b00620a8a2f51870e9c0e59e8c
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2020-03-28 22:49:19 +0100

    xml reader

---
+++ xml/xml_reader.py
@@ -0,0 +1,49 @@
+import os
+
+from xml.etree import ElementTree
+
+from xml.etree import ElementTree
+root = ElementTree.parse("1.xml").getroot()
+
+def tager():
+    sentence = ''
+    result = dict()
+    tag = ()
+    part = []
+    for item in root.findall("a"):
+        if item.attrib['title'] == 'ORG':
+            children = item.getchildren()
+            for child in children:
+                sentence = sentence + " " + str(child.text)
+                tag = ('ORG', child.text)
+                part.extend(tag)
+        if item.attrib['title'] == 'LOC':
+            children = item.getchildren()
+            for child in children:
+                sentence = sentence + " " + str(child.text)
+                tag = ('LOC', child.text)
+                part.extend(tag)
+        elif item.attrib['title'] == 'TTL':
+            children = item.getchildren()
+            for child in children:
+                sentence = sentence + " " + str(child.text)
+                tag = ('TTL', child.text)
+                part.extend(tag)
+        elif item.attrib['title'] == 'TIME':
+            children = item.getchildren()
+            for child in children:
+                sentence = sentence + " " + str(child.text)
+                tag = ('TIME', child.text)
+                part.extend(tag)
+        elif item.attrib['title'] == 'PER':
+            children = item.getchildren()
+            for child in children:
+                sentence = sentence + " " + str(child.text)
+                tag = ('PER', child.text)
+                part.extend(tag)
+
+    result[sentence] = part
+    print(result)
+
+tager()
+    
