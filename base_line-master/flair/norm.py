3f3f3bf5d294623f06a391a98c3716e7ca2349ad
Hizkiel Alemayehu <44091398+Hizkie@users.noreply.github.com>
2020-06-25 00:59:15 +0200

    normalilzation

--- base_line-master/flair/norm.py
+++ base_line-master/flair/norm.py
@@ -85,8 +85,8 @@ def normalize(norm):
     return norm
 
 
-with open('sst_train.txt','r',encoding='utf8') as fin:
-    with open('sst_train2.txt', 'w', encoding='utf8', newline="") as f:
+with open('to positive/sst_dev.txt','r',encoding='utf8') as fin:
+    with open('to positive/sst_dev2.txt', 'w', encoding='utf8', newline="") as f:
          for line in fin:
              f.write(normalize(line))
 
